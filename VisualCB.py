4f46e4febe8c8b9573827a36b77daf252d5d21ac
Hosi M. Chauke <HMChaukeDC@gmail.com>
2025-11-09 23:15:03 +0200

    changed some stuff, acceleration tested,X is screwed up

--- VisualCB.py
+++ VisualCB.py
@@ -10,7 +10,7 @@ pg.display.set_caption('CELESTIAL BODIES DEMO')
 SpaceImage1 = pg.image.load('Space2.jpeg').convert_alpha()
 SpaceImage1 = pg.transform.scale(SpaceImage1, (500, 500))
 G = 6.674 * pow(10,-11)
-Distance_Scale=332500
+Distance_Scale=747989.355
 
 class Planet:
     def __init__(self, Name:str, Mass:float, Radius:float, Xposition:float, Yposition:float, VeloX:float, VeloY:float, Scale:float):       
@@ -47,6 +47,7 @@ class Planet:
         Acc = (G*other.Mass)/pow(Radius*Distance_Scale,2)
         XAcc = (math.cos(angle)*Acc)/Distance_Scale
         self.VeloX+=XAcc
+        self.XAcc = XAcc
          
     def YAccDueTo(self,other:Planet):
         Radius = math.sqrt ((pow((self.Xposition-other.Xposition),2)+pow((self.Yposition-other.Yposition),2))) 
@@ -57,9 +58,7 @@ class Planet:
         Acc = (G*other.Mass)/pow(Radius*Distance_Scale,2)
         YAcc = (math.sin(angle)*Acc)/Distance_Scale
         self.VeloY+=YAcc
-         
-    #def Accelerate(self, AccDueTo(other)):
-        
+        self.YAcc = YAcc
         
     def Create(self,Colour):
         if (self.VeloX < 0) or (self.VeloX >0):
@@ -78,28 +77,30 @@ class Planet:
     
         
 print("--[TEST AREA]--")  
-d = Planet("Sun", 1989100, 695950, 250, 250,0.0,0.0,19000)
-print(f"Radius of {d.Name} is {d.Radius}")
-print(f"Mass of {d.Name} is {d.Mass}")
-print(f"Original Coordinates of {d.Name} are ({d.Xposition},{d.Yposition})")
-e = Planet("Earth", 5.97, 6371, 50, 250,0.0,0.0,350)
-print(f"Radius of {d.Name} is {d.Radius}")
-print(f"Mass of {d.Name} is {d.Mass}")
-print(f"Original Coordinates of {d.Name} are ({d.Xposition},{d.Yposition})")
+Su = Planet("Sun", 1989100, 695950, 250, 250,0.0,0.0,19000)
+print(f"Radius of {Su.Name} is {Su.Radius}")
+print(f"Mass of {Su.Name} is {Su.Mass}")
+print(f"Original Coordinates of {Su.Name} are ({Su.Xposition},{Su.Yposition})")
+Er = Planet("Earth", 5.97, 6371, 50, 250,-0.25,-.737,360)
+print(f"Radius of {Er.Name} is {Er.Radius}")
+print(f"Mass of {Er.Name} is {Er.Mass}")
+print(f"Original Coordinates of {Er.Name} are ({Er.Xposition},{Er.Yposition})")
 
 print("--[TEST AREA]--")    
-
+print("--[LIVE AREA]--")   
 while True:
     window.blit(SpaceImage1,(0,0))
-    d.Create((225,225,0))  
-    e.Create((73,157,208))
-    e.XAccDueTo(d)
-    e.YAccDueTo(d)
-    d.XAccDueTo(e)
-    d.YAccDueTo(e)
+    Su.Create((225,225,0))  
+    Er.Create((73,157,208))
+    Er.XAccDueTo(Su)
+    Er.YAccDueTo(Su)
+    Su.XAccDueTo(Er)
+    Su.YAccDueTo(Er)
+    print(f"{Er.XAcc}p/f^2   |{Er.YAcc}p/f^2")
     for event in pg.event.get():
         if event.type == pg.QUIT:
             pg.quit()
             exit()
     pg.display.update()
-    Clock.tick(30)
+    Clock.tick(60)
+print("--[LIVE AREA]--")   

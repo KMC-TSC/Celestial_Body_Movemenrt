514eb82652203d15c2892c1768595deb283273de
Hosi M. Chauke <HMChaukeDC@gmail.com>
2025-11-09 14:29:34 +0200

    added scaling

--- VisualCB.py
+++ VisualCB.py
@@ -10,23 +10,21 @@ pg.display.set_caption('CELESTIAL BODIES DEMO')
 SpaceImage1 = pg.image.load('Space2.jpeg').convert_alpha()
 SpaceImage1 = pg.transform.scale(SpaceImage1, (500, 500))
 G = 6.674 * pow(10,-11)
+Bodies=[]
 
 class Planet:
-    def __init__(self, Name:str, Mass:float, Radius:float, Xposition:float, Yposition:float, VeloX:float, VeloY:float):       
+    def __init__(self, Name:str, Mass:float, Radius:float, Xposition:float, Yposition:float, VeloX:float, VeloY:float, Scale:float):       
         self.Name = Name
         self.Mass = Mass
-        self.Radius = Radius
+        self.Radius = Radius 
         self.Xposition = Xposition
         self.Yposition = Yposition
         self.VeloX = VeloX
         self.VeloY = VeloY
+        self.Scale = Scale
         self.update_text()
+        Bodies.append(self)
         
-
-    def update_text(self):
-        font_size = max(15, int(self.Radius * 0.9))  # Prevent font size from being too small
-        TextFont = pg.font.Font(None, font_size)
-        self.text = TextFont.render(self.Name, True, 'White')
 
     def ChangeRadius(self, NewRadius:float):
         self.Radius= NewRadius
@@ -51,17 +49,25 @@ class Planet:
     def Create(self,Colour):
         if (self.VeloX < 0) or (self.VeloX >0):
             self.Xposition += self.VeloX
+        if (self.VeloY < 0) or (self.VeloY >0):
+            self.Yposition += self.VeloY
         self.Colour = Colour
-        pg.draw.circle(window,(self.Colour),(self.Xposition,self.Yposition),self.Radius)
-        window.blit(self.text, (self.Xposition-(self.Radius/2), self.Yposition-self.Radius-30))
+        pg.draw.circle(window,(self.Colour),(self.Xposition,self.Yposition),self.Radius/self.Scale)
+        window.blit(self.text, (self.Xposition-((self.Radius/self.Scale)/2), self.Yposition-(self.Radius/self.Scale)-30))
+
+    def update_text(self):
+        font_size = max(15, int((self.Radius/self.Scale) * 0.9))  # Prevent font size from being too small
+        TextFont = pg.font.Font(None, font_size)
+        self.text = TextFont.render(self.Name, True, 'White')
 
     
         
 print("--[TEST AREA]--")  
-d = Planet("Sun", 120000, 10, 100, 200,3,0)
+d = Planet("Sun", 120000, 695950, 100, 200,0.5,0.7,19000)
 print(f"Radius of {d.Name} is {d.Radius}")
 print(f"Mass of {d.Name} is {d.Mass}")
 print(f"Original Coordinates of {d.Name} are ({d.Xposition},{d.Yposition})")
+print(Bodies)
 
 print("--[TEST AREA]--")    
 
